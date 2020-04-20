df78f684f2ebb9c9890489c5e5ac6f774b048841
TTOzzi <devttozzi@gmail.com>
2020-04-20 22:30:00 +0900

    [#3] Feat: SectionHeader 커스텀클래스 구현
    키워드와 제목 표시
    오토레이아웃으로 위치를 잡아줌
    테스트를 위해 ViewController 에 헤더 생성 코드 작성

--- iOS/SideDish/SideDish/ViewController.swift
+++ iOS/SideDish/SideDish/ViewController.swift
@@ -12,7 +12,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var menuTableView: UITableView!
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+        menuTableView.delegate = self
+        menuTableView.dataSource = self
+        
+        menuTableView.register(MenuSectionHeader.self, forHeaderFooterViewReuseIdentifier: MenuSectionHeader.reuseIdentifier)
     }
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = tableView.dequeueReusableHeaderFooterView(withIdentifier:
+            MenuSectionHeader.reuseIdentifier) as! MenuSectionHeader
+        header.keywordLabel.text = "밑반찬"
+        header.sectionTitle.text = "언제 먹어도 든든한 밑반찬"
+        return header
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return MenuSectionHeader.height
+    }
+}
