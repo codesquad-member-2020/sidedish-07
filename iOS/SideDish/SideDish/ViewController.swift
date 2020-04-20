25c5f87844432af6f8eb751db3879283921ed4a0
TTOzzi <devttozzi@gmail.com>
2020-04-20 18:54:15 +0900

    [#3] Feat: TableView 생성
    TableView 생성 후 IBOutlet 으로 ViewController 와 연결

--- iOS/SideDish/SideDish/ViewController.swift
+++ iOS/SideDish/SideDish/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var menuTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
