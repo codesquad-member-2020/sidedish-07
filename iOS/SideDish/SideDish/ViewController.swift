15e99da7a34078f9bbc25065c77d9a9cabc27079
TTOzzi <devttozzi@gmail.com>
2020-04-20 18:49:45 +0900

    [#3] Feat: 프로젝트 생성
    프로젝트 생성 후 불필요 코드 제거

---
+++ iOS/SideDish/SideDish/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  SideDish
+//
+//  Created by TTOzzi on 2020/04/20.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
