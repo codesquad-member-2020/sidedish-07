27a0c84d77f43aa04fbf932254d28cead589d0ce
Jay <beginin15@gmail.com>
2020-04-22 01:57:14 +0900

    [#8] Chore: 스키마 변경, data.sql 추가

---
+++ BE/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO delivery (name) values ('새벽배송');
+INSERT INTO delivery (name) values ('전국택배');
+
+INSERT INTO badge (name) values ('이벤트특가');
+INSERT INTO badge (name) values ('론칭특가');
+INSERT INTO badge (name) values ('베스트');
