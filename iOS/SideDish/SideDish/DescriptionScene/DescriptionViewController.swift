f7c2ab3e2b1471ecda12c040e45ef2ef9ff09d04
TTOzzi <devttozzi@gmail.com>
2020-04-21 21:52:51 +0900

    [#12] Feat: 상세페이지 뷰 컨트롤러 생성

---
+++ iOS/SideDish/SideDish/DescriptionScene/DescriptionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DescriptionViewController.swift
+//  SideDish
+//
+//  Created by TTOzzi on 2020/04/21.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import UIKit
+
+class DescriptionViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
