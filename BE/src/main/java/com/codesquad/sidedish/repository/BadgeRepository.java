4a947d215ebff7d0db740fa6518e8b5eb9a6721b
Jay <beginin15@gmail.com>
2020-04-22 02:01:00 +0900

    [#8] Feat: 기본적인 CRUD를 위한 레포지토리 클래스 생성
    - Badge, Delivery는 Product와 별개의 어그리게이트 그룹이라고 취급. 왜냐하면 Product 내용이 변경된다고 해서 Badge 데이터가 바뀌지 않으므로.
    - 이미지 관련 레포지토리는 고민이 필요함. 지금은 단순 save를 위해서 레포지토리 클래스를 정의한 것.

---
+++ BE/src/main/java/com/codesquad/sidedish/repository/BadgeRepository.java
@@ -0,0 +1,7 @@
+package com.codesquad.sidedish.repository;
+
+import com.codesquad.sidedish.entity.Badge;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BadgeRepository extends CrudRepository<Badge, Integer> {
+}
---
+++ BE/src/main/java/com/codesquad/sidedish/repository/ThumbImageRepository.java
@@ -0,0 +1,7 @@
+package com.codesquad.sidedish.repository;
+
+import com.codesquad.sidedish.entity.ThumbImage;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ThumbImageRepository extends CrudRepository<ThumbImage, Integer> {
+}
