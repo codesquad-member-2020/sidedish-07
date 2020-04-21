c50c9f726e653d7674919324ab59b8bf745df0ca
Jay <beginin15@gmail.com>
2020-04-22 01:57:14 +0900

    [#8] Chore: 스키마 변경, data.sql 추가

--- BE/src/main/resources/schema.sql
+++ BE/src/main/resources/schema.sql
@@ -9,12 +9,15 @@ DROP TABLE IF EXISTS product_badge;
 DROP TABLE IF EXISTS product_delivery;
 
 CREATE TABLE product (
-    hash INT auto_increment primary key ,
-    menu enum ('main', 'soup', 'side') ,
-    title varchar(64) not null ,
+    id INT auto_increment primary key ,
+    hash varchar(5) unique not null ,
+    menu enum ('MAIN', 'SOUP', 'SIDE') ,
+    title varchar(64) ,
     description varchar(64) not null ,
     s_price INT ,
-    n_price INT not null
+    n_price INT not null ,
+    delivery_fee varchar(64) not null ,
+    delivery_possible varchar(64) not null
 );
 
 CREATE TABLE category (
@@ -35,32 +38,30 @@ CREATE TABLE delivery (
 CREATE TABLE thumb_image (
     id INT auto_increment primary key ,
     url varchar(128) not null ,
-    product INT references product(hash)
+    product varchar(5) references product(id)
 );
 
 CREATE TABLE detail_image (
     id INT auto_increment primary key ,
     url varchar(128) not null ,
-    product INT references product(hash)
+    product varchar(5) references product(id)
 );
 
 -- m:n 맵핑을 위한 테이블
 CREATE TABLE product_category (
-    product INT references product(hash) ,
+    product INT references product(id) ,
     categroy INT references category(id) ,
     PRIMARY KEY (product, categroy)
 );
 
 CREATE TABLE product_badge (
-    product INT references product(hash) ,
+    product INT references product(id) ,
     badge INT references badge(id) ,
     PRIMARY KEY (product, badge)
 );
 
 CREATE TABLE product_delivery (
-    product INT references product(hash) ,
-    badge INT references badge(id) ,
-    fee INT not null ,
-    info varchar(62) not null ,
-    PRIMARY KEY (product, badge)
+    product INT references product(id) ,
+    delivery INT references delivery(id) ,
+    PRIMARY KEY (product, delivery)
 );
