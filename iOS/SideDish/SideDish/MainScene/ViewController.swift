eed5b430162b7c2583f99fb067dfe1796348a4d6
TTOzzi <devttozzi@gmail.com>
2020-04-22 23:09:22 +0900

    [#17] Feat: Mock API 에서 데이터를 받아와 메인화면 표시 구현
    기본제공된 Mock API 에서 든든한반찬 부분만 받아와 표시
    
    이것저것 수정해야 할것이 많음. 리팩토링 필요
    이미지가 완전히 둥글게 표시되지 않는 문제 -> autolayout 으로 제약조건을 설정해서 그런지 frame.width 가 실제 width 와 다른듯함

--- iOS/SideDish/SideDish/MainScene/ViewController.swift
+++ iOS/SideDish/SideDish/MainScene/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var menuTableView: UITableView!
     
+    var data: [SideDish]?
+    
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.setNavigationBarHidden(true, animated: animated)
         super.viewWillAppear(animated)
@@ -21,22 +23,47 @@ class ViewController: UIViewController {
         menuTableView.dataSource = self
         
         menuTableView.register(MenuSectionHeader.self, forHeaderFooterViewReuseIdentifier: MenuSectionHeader.reuseIdentifier)
+        
+        NetworkManager.httpRequest(url: NetworkManager.serverUrl + "main", method: .GET) { (data, _, _) in
+            guard let data = data else { return }
+            let decoder = JSONDecoder()
+            do {
+                let decodedData = try decoder.decode(SideDishData.self, from: data)
+                self.data = decodedData.body
+                DispatchQueue.main.async {
+                    self.menuTableView.reloadData()
+                }
+            } catch {
+                
+            }
+        }
     }
 }
 
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return data?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: MenuTableViewCell.reuseIdentifier) as? MenuTableViewCell else { return UITableViewCell() }
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MenuTableViewCell.reuseIdentifier) as? MenuTableViewCell,
+            let data = data else { return UITableViewCell() }
+        let sideDish = data[indexPath.row]
+        do {
+            let imageData = try Data(contentsOf: sideDish.image)
+            cell.menuImage.image = UIImage(data: imageData)
+        } catch {
+            print(error)
+        }
+        cell.titleLabel.text = sideDish.title
+        cell.descriptionLabel.text = sideDish.description
+        cell.priceLabel.text = sideDish.s_price
         return cell
     }
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let header = tableView.dequeueReusableHeaderFooterView(withIdentifier:
-            MenuSectionHeader.reuseIdentifier) as! MenuSectionHeader
+        guard let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: MenuSectionHeader.reuseIdentifier) as? MenuSectionHeader else { return nil }
+        
         header.keywordLabel.text = "밑반찬"
         header.sectionTitle.text = "언제 먹어도 든든한 밑반찬"
         return header
