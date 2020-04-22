d5427c27ca12d3d7145c70a52d9fbdca0406a229
Jay <beginin15@gmail.com>
2020-04-22 21:56:54 +0900

    [#8] Chore: data.sql 제거
    - 직접 데이터 insert하는 것은 모두 제거하고, api 요청 데이터로만 DB에 저장하도록 변경

--- BE/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO delivery (name) values ('새벽배송');
-INSERT INTO delivery (name) values ('전국택배');
-
-INSERT INTO badge (name) values ('이벤트특가');
-INSERT INTO badge (name) values ('론칭특가');
-INSERT INTO badge (name) values ('베스트');
