b3787079cb0015be5093b67c7b787fa78c2643ac
Jay <beginin15@gmail.com>
2020-04-22 21:59:54 +0900

    [#8] Feat: 엔티티에서 필요한 생성자를 수정, getter 정의
    - api로부터 데이터를 생성하는 엔티티의 경우, 필요에 따라 생성자 정의
    - 응답 객체(JSON)로 데이터를 확인하기 위한 getter 정의

--- BE/src/main/java/com/codesquad/sidedish/entity/ProductBadge.java
+++ BE/src/main/java/com/codesquad/sidedish/entity/ProductBadge.java
@@ -4,4 +4,11 @@ public class ProductBadge {
 
     private Integer badge;
 
+    public ProductBadge(Integer badge) {
+        this.badge = badge;
+    }
+
+    public Integer getBadge() {
+        return badge;
+    }
 }
