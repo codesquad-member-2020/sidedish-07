81599acc65382e1210e9d7ed95e8f569fe07f28b
Jay <beginin15@gmail.com>
2020-04-22 22:06:14 +0900

    [#8] Feat: best, detail, side, main, soup api로 파싱한 데이터를 DB에 저장
    - /best 응답의 name을 이용하여 Category 객체 생성 및 DB에 저장
    - /detail 응답의 데이터를 이용하여 Product 객체 생성 및 DB에 저장
    - /side, /main, /soup 응답 데이터를 이용하여 DB에 저장된 Product의 Menu와 Badge 정보를 update
    - /detail에서 생성 및 저장했던 Delivery 정보를 /side, /main, /soup에서 작업하도록 변경
    - DB에 저장된 Category 별로 랜덤으로 가져온 Product 3개씩 관계를 맵핑

--- BE/src/main/java/com/codesquad/sidedish/repository/BadgeRepository.java
+++ BE/src/main/java/com/codesquad/sidedish/repository/BadgeRepository.java
@@ -1,7 +1,13 @@
 package com.codesquad.sidedish.repository;
 
 import com.codesquad.sidedish.entity.Badge;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface BadgeRepository extends CrudRepository<Badge, Integer> {
+
+    @Query("SELECT b.id, b.name FROM badge b WHERE b.name = :name")
+    Optional<Badge> findByName(String name);
 }
