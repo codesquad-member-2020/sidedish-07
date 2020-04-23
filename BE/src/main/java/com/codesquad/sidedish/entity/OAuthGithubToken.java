ad959771e7f6ff85af5e84f85150366eafa2a6d4
sedin2 <53131108+sedin2@users.noreply.github.com>
2020-04-23 22:14:13 +0900

    [#13] Feat: Access Token 발급 기능 구현
    - 사용자가 github에 로그인 -> github에서 code를 받음
    - 서버에서 code, client_id, client_secret를 헤더에 담아 github에 요청
    - github에서 access token을 발급 서버는 사용자에게 aceess token을 반환

--- BE/src/main/java/com/codesquad/sidedish/entity/OAuthGithubToken.java
+++ BE/src/main/java/com/codesquad/sidedish/entity/OAuthGithubToken.java
@@ -1,4 +1,45 @@
 package com.codesquad.sidedish.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.stereotype.Component;
+
+@Component
 public class OAuthGithubToken {
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("scope")
+    private String scope;
+
+    @JsonProperty("token_type")
+    private String tokenType;
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
+
+    public String getAuthorization() {
+        return this.tokenType + " " + this.accessToken;
+    }
 }
