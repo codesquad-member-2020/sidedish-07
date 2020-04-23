cb1045d6584c8a0ca5edca26f371d440ab05f801
sedin2 <53131108+sedin2@users.noreply.github.com>
2020-04-23 22:14:13 +0900

    [#13] Feat: Access Token 발급 기능 구현
    - 사용자가 github에 로그인 -> github에서 code를 받음
    - 서버에서 code, client_id, client_secret를 헤더에 담아 github에 요청
    - github에서 access token을 발급 서버는 사용자에게 aceess token을 반환

--- BE/src/main/java/com/codesquad/sidedish/api/LoginController.java
+++ BE/src/main/java/com/codesquad/sidedish/api/LoginController.java
@@ -1,11 +1,14 @@
 package com.codesquad.sidedish.api;
 
+import com.codesquad.sidedish.entity.OAuthGithubToken;
 import com.codesquad.sidedish.service.OAuthService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.websocket.server.PathParam;
 
 @RestController
@@ -20,9 +23,12 @@ public class LoginController {
     }
 
     @GetMapping("/login/oauth2/code/github")
-    public String OauthTest(@PathParam("code") String code) {
+    public ResponseEntity<String> OauthTest(@PathParam("code") String code, HttpServletResponse response) {
         log.debug("{}", code);
-        oAuthService.getAccessToken(code);
-        return "login Success";
+        OAuthGithubToken oAuthGithubToken = oAuthService.getAccessToken(code);
+        response.setHeader("Authorization", oAuthGithubToken.getAuthorization());
+        log.debug("{}", oAuthGithubToken.getAuthorization());
+
+        return ResponseEntity.ok("Login Success");
     }
 }
