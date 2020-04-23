0ad4b96d34f951a8fc958e56ba452e0114430544
Jay <beginin15@gmail.com>
2020-04-23 18:17:31 +0900

    [#23] Refactor: 메뉴별 api 요청을 하나의 메소드로 처리하도록 변경
    - 컨트롤러의 메소드가 main, side, soup에 모두 대응할 수 있도록 변경

--- BE/src/main/java/com/codesquad/sidedish/api/SidedishController.java
+++ BE/src/main/java/com/codesquad/sidedish/api/SidedishController.java
@@ -6,11 +6,14 @@ import com.codesquad.sidedish.entity.Menu;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/products")
 public class SidedishController {
 
     private static final Logger log = LoggerFactory.getLogger(SidedishController.class);
@@ -20,8 +23,8 @@ public class SidedishController {
         this.productDAO = productDAO;
     }
 
-    @GetMapping("/main")
-    public List<SimpleDTO> showProductsByMenu() {
-        return productDAO.selectByMenu(Menu.MAIN);
+    @GetMapping("/{menu}")
+    public List<SimpleDTO> showProductsByMenu(@PathVariable String menu) {
+        return productDAO.selectByMenu(Menu.valueOf(menu.toUpperCase()));
     }
 }
