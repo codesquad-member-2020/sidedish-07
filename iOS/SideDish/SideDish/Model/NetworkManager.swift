3e03fa29bc9bfa24a9b68b78757ee3a4fc19d02f
TTOzzi <devttozzi@gmail.com>
2020-04-23 23:32:49 +0900

    [#17] Refactor: 네트워크 통신부분 분리
    DataManager 객체를 만들어 API 에서 데이터를 받아와 저장하는 역할을 하도록 해줌

--- iOS/SideDish/SideDish/Model/NetworkManager.swift
+++ iOS/SideDish/SideDish/Model/NetworkManager.swift
@@ -17,7 +17,6 @@ class NetworkManager {
     }
     
     static let session = URLSession.shared
-    static let serverUrl = "https://h3rb9c0ugl.execute-api.ap-northeast-2.amazonaws.com/develop/baminchan/"
     
     static func httpRequest(url: String, method: HTTPMethod, body: Data? = nil, completionHandler: @escaping (Data?, URLResponse?, Error?) -> ()) {
         guard let url = URL(string: url) else { return }
