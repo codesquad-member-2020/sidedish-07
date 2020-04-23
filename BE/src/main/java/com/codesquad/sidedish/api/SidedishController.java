a1c99c7a9a7843c240b9cfdd33c6e6a47e3f89a1
Jay <beginin15@gmail.com>
2020-04-23 21:52:05 +0900

    [#27] Feat: 상세 페이지 요청 API 구현 및 예외 처리 응답 구현
    - 요청 URL이 잘못된 경우, 400 코드와 함께 실패 메세지 전달하도록 변경
    - 쿼리 결과가 0개인 경우 DataAccessException 에러가 발생하므로 catch에서 에러 응답 객체로 반환
    - 배송 타입에 따라서 deliveryInfo 데이터를 가공. 리팩토링이 필요해보임

--- BE/src/main/java/com/codesquad/sidedish/api/SidedishController.java
+++ BE/src/main/java/com/codesquad/sidedish/api/SidedishController.java
@@ -1,10 +1,16 @@
 package com.codesquad.sidedish.api;
 
 import com.codesquad.sidedish.dao.ProductDAO;
+import com.codesquad.sidedish.dto.DetailDTO;
 import com.codesquad.sidedish.dto.SimpleDTO;
 import com.codesquad.sidedish.entity.Menu;
+import com.codesquad.sidedish.exception.ResourceNotFound;
+import com.codesquad.sidedish.response.ResponseData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +30,46 @@ public class SidedishController {
     }
 
     @GetMapping("/{menu}")
-    public List<SimpleDTO> showProductsByMenu(@PathVariable String menu) {
-        return productDAO.selectByMenu(Menu.valueOf(menu.toUpperCase()));
+    public ResponseEntity<ResponseData> showProductsByMenu(@PathVariable String menu) {
+        List<SimpleDTO> products = null;
+        try {
+            products = productDAO.selectByMenu(Menu.valueOf(menu.toUpperCase()));
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(new ResponseData(ResponseData.STATUS.ERROR, "API URL 오류"), HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(new ResponseData(ResponseData.STATUS.SUCCESS, products), HttpStatus.OK);
+    }
+
+    @GetMapping("/detail/{hash}")
+    public ResponseEntity<ResponseData> showDetail(@PathVariable String hash) {
+        DetailDTO product = null;
+        try {
+            product = productDAO.selectDetailByHash(hash);
+            processDeliveryInfo(product);
+        } catch (DataAccessException e) {
+            return new ResponseEntity<>(new ResponseData(ResponseData.STATUS.ERROR, "존재하지 않는 상품입니다."), HttpStatus.NOT_FOUND);
+        } catch (ResourceNotFound e) {
+            return new ResponseEntity<>(new ResponseData(ResponseData.STATUS.ERROR, e.getMessage()), HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(new ResponseData(ResponseData.STATUS.SUCCESS, product), HttpStatus.OK);
+    }
+
+    private void processDeliveryInfo(DetailDTO product) {
+        boolean isDoubleType = false;
+        StringBuilder deliveryInfo = new StringBuilder();
+        for (String deliveryType : product.getDeliveryTypes()) {
+            if (isDoubleType)
+                deliveryInfo.append(" / ");
+
+            if (deliveryType.equals("새벽배송")) {
+                deliveryInfo.append("서울 경기 새벽배송");
+                isDoubleType = true;
+            }
+            if (deliveryType.equals("전국택배")) {
+                deliveryInfo.append("전국택배 (제주 및 도서산간 불가)");
+                isDoubleType = true;
+            }
+        }
+        product.setDeliveryInfo(deliveryInfo.append(product.getDeliveryInfo()).append(" 수령 가능한 상품입니다.").toString());
     }
 }
