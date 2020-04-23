132de234dce772ab40aab52924c004e45f609c3f
Jay <beginin15@gmail.com>
2020-04-23 21:52:05 +0900

    [#27] Feat: 상세 페이지 요청 API 구현 및 예외 처리 응답 구현
    - 요청 URL이 잘못된 경우, 400 코드와 함께 실패 메세지 전달하도록 변경
    - 쿼리 결과가 0개인 경우 DataAccessException 에러가 발생하므로 catch에서 에러 응답 객체로 반환
    - 배송 타입에 따라서 deliveryInfo 데이터를 가공. 리팩토링이 필요해보임

--- BE/src/main/java/com/codesquad/sidedish/dao/ProductDAO.java
+++ BE/src/main/java/com/codesquad/sidedish/dao/ProductDAO.java
@@ -1,14 +1,18 @@
 package com.codesquad.sidedish.dao;
 
+import com.codesquad.sidedish.dto.DetailDTO;
 import com.codesquad.sidedish.dto.SimpleDTO;
 import com.codesquad.sidedish.entity.Menu;
+import com.codesquad.sidedish.exception.ResourceNotFound;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class ProductDAO {
@@ -22,20 +26,37 @@ public class ProductDAO {
 
     public List<SimpleDTO> selectByMenu(Menu menu) {
         List<SimpleDTO> products = queryFromProductByMenu(menu);
-        String sqlForDeliveryTypes = "SELECT d.name as name" +
+        final String sqlForDeliveryTypes = "SELECT d.name as name" +
                 " FROM product_delivery pd" +
                 " LEFT OUTER JOIN delivery d ON pd.delivery = d.id" +
                 " WHERE product = :productId";
-        String sqlForBadges = "SELECT b.name as name" +
+        final String sqlForBadges = "SELECT b.name as name" +
                 " FROM product_badge pb" +
                 " LEFT OUTER JOIN badge b ON pb.badge = b.id" +
                 " WHERE product = :productId";
-
         for (SimpleDTO product : products) {
-            product.setDeliveryTypes(queryByProductId(sqlForDeliveryTypes, product.getId()));
-            product.setBadges(queryByProductId(sqlForBadges, product.getId()));
+            product.setDeliveryTypes(queryByProductId(sqlForDeliveryTypes, product.getId(), "name"));
+            product.setBadges(queryByProductId(sqlForBadges, product.getId(), "name"));
         }
         return products;
+    }
+
+    public DetailDTO selectDetailByHash(String hash) throws ResourceNotFound {
+        DetailDTO product = queryFromProductByHash(hash).orElseThrow(ResourceNotFound::new);
+        final String sqlForThumbImages = "SELECT ti.url as url" +
+                " FROM thumb_image ti" +
+                " WHERE ti.product = :productId";
+        final String sqlForDetailImages = "SELECT di.url as url" +
+                " FROM detail_image di" +
+                " WHERE di.product = :productId";
+        final String sqlForDeliveryTypes = "SELECT d.name as name" +
+                " FROM product_delivery pd" +
+                " LEFT OUTER JOIN delivery d ON pd.delivery = d.id" +
+                " WHERE product = :productId";
+        product.setThumbImages(queryByProductId(sqlForThumbImages, product.getId(), "url"));
+        product.setDetailImages(queryByProductId(sqlForDetailImages, product.getId(), "url"));
+        product.setDeliveryTypes(queryByProductId(sqlForDeliveryTypes, product.getId(), "name"));
+        return product;
     }
 
     private List<SimpleDTO> queryFromProductByMenu(Menu menu) {
@@ -58,11 +79,38 @@ public class ProductDAO {
         );
     }
 
-    private List<String> queryByProductId(String sql, Integer productId) {
+    private Optional<DetailDTO> queryFromProductByHash(String hash) throws DataAccessException {
+        final String sql = "SELECT id, hash, title, description, CONCAT(FORMAT(s_price, 0), '원') as s_price_currency_format, CONCAT(FORMAT(n_price, 0), '원') as n_price_currency_format, s_price, n_price, delivery_fee, delivery_possible, image" +
+                " FROM product" +
+                " WHERE hash = :hash";
+        return namedParameterJdbcTemplate.queryForObject(sql,
+                new MapSqlParameterSource("hash", hash),
+                (rs, rowNum) ->
+                        Optional.of( new DetailDTO(
+                                rs.getInt("id"),
+                                rs.getString("hash"),
+                                rs.getString("title"),
+                                rs.getString("description"),
+                                rs.getString("s_price_currency_format"),
+                                rs.getString("n_price_currency_format"),
+                                getPoint(rs.getInt("s_price"), rs.getInt("n_price")),
+                                rs.getString("delivery_fee"),
+                                rs.getString("delivery_possible"),
+                                rs.getString("image")
+                        ))
+        );
+    }
+
+    private List<String> queryByProductId(String sql, Integer productId, String namedParameter) {
         return namedParameterJdbcTemplate.query(sql,
                 new MapSqlParameterSource("productId", productId),
                 (rs, rowNum) ->
-                        new String(rs.getString("name"))
+                        new String(rs.getString(namedParameter))
         );
+    }
+
+    private String getPoint(int sPrice, int nPrice) {
+        final float POINT_RATIO = 0.01f;
+        return sPrice == 0 ? (int)(nPrice * POINT_RATIO) + "원" : (int)(sPrice * POINT_RATIO) + "원";
     }
 }
--- BE/src/main/java/com/codesquad/sidedish/dto/DetailDTO.java
+++ BE/src/main/java/com/codesquad/sidedish/dto/DetailDTO.java
@@ -1,8 +1,13 @@
 package com.codesquad.sidedish.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.util.List;
 
 public class DetailDTO {
+
+    @JsonIgnore
+    private Integer id;
 
     private String hash;
 
@@ -14,13 +19,99 @@ public class DetailDTO {
 
     private String normalPrice;
 
+    private String point;
+
     private String deliveryFee;
 
-    private String deliveryPossible;
+    private String deliveryInfo;
 
     private String topImage;
 
     private List<String> thumbImages;
 
     private List<String> detailImages;
+
+    @JsonIgnore
+    private List<String> deliveryTypes;
+
+    public DetailDTO(Integer id, String hash, String title, String description, String salePrice, String normalPrice, String point, String deliveryFee, String deliveryInfo, String topImage) {
+        this.id = id;
+        this.hash = hash;
+        this.title = title;
+        this.description = description;
+        this.salePrice = salePrice;
+        this.normalPrice = normalPrice;
+        this.point = point;
+        this.deliveryFee = deliveryFee;
+        this.deliveryInfo = deliveryInfo;
+        this.topImage = topImage;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getSalePrice() {
+        return salePrice;
+    }
+
+    public String getNormalPrice() {
+        return normalPrice;
+    }
+
+    public String getPoint() {
+        return point;
+    }
+
+    public String getDeliveryFee() {
+        return deliveryFee;
+    }
+
+    public String getDeliveryInfo() {
+        return deliveryInfo;
+    }
+
+    public void setDeliveryInfo(String deliveryInfo) {
+        this.deliveryInfo = deliveryInfo;
+    }
+
+    public String getTopImage() {
+        return topImage;
+    }
+
+    public List<String> getThumbImages() {
+        return thumbImages;
+    }
+
+    public void setThumbImages(List<String> thumbImages) {
+        this.thumbImages = thumbImages;
+    }
+
+    public List<String> getDetailImages() {
+        return detailImages;
+    }
+
+    public void setDetailImages(List<String> detailImages) {
+        this.detailImages = detailImages;
+    }
+
+    public List<String> getDeliveryTypes() {
+        return deliveryTypes;
+    }
+
+    public void setDeliveryTypes(List<String> deliveryTypes) {
+        this.deliveryTypes = deliveryTypes;
+    }
 }
