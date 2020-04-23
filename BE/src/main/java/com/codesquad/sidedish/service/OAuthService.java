8c9f8271ee9920ccd7be2e6ddf252c5f2310bd6d
sedin2 <53131108+sedin2@users.noreply.github.com>
2020-04-23 21:39:51 +0900

    [#13] Feat: Access Token을 얻어오기 위해 RestTemplate 적용
    - code를 받아와 Access Token 발급을 위한 POST 요청
    - Client Secret Key 암호화가 필요 (git에 그대로 노출)

--- BE/src/main/java/com/codesquad/sidedish/service/OAuthService.java
+++ BE/src/main/java/com/codesquad/sidedish/service/OAuthService.java
@@ -1,8 +1,43 @@
 package com.codesquad.sidedish.service;
 
+import com.codesquad.sidedish.entity.OAuthGithubToken;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class OAuthService {
+
+    private static final Logger log = LoggerFactory.getLogger(OAuthService.class);
+
+    private final String CLIENTID = "71186054709e9adda0f9";
+    private final String CLIENTSECRET = "c0195e8d988b81aee4d7565da58941dd8e8fcc5a";
+    private final String URL = "https://github.com/login/oauth/access_token";
+
+    public void getAccessToken(String code) {
+        MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>();
+        Map map = new HashMap<String, String>();
+        map.put("Content-Type", "application/json");
+
+        headers.setAll(map);
+
+        Map req_payload = new HashMap();
+        req_payload.put("code", code);
+        req_payload.put("client_id", CLIENTID);
+        req_payload.put("client_secret", CLIENTSECRET);
+
+        HttpEntity<?> request = new HttpEntity<>(req_payload, headers);
+
+        ResponseEntity<?> response = new RestTemplate().postForEntity(URL, request, String.class);
+        log.debug("{}", response.getBody());
+    }
 
 }
