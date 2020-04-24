aa18ac51994a6c01110f6d6f98c8d51b0f0e6911
sedin <leesejinn@naver.com>
2020-04-25 00:05:47 +0900

    [#33] Feat: Access Token을 통해 githubEmail 받는 기능 구현
    - OAuthService에서 처리하도록 로직을 구현

--- BE/src/main/java/com/codesquad/sidedish/service/OAuthService.java
+++ BE/src/main/java/com/codesquad/sidedish/service/OAuthService.java
@@ -1,10 +1,13 @@
 package com.codesquad.sidedish.service;
 
 import com.codesquad.sidedish.entity.OAuthGithubToken;
+import com.codesquad.sidedish.entity.User;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
@@ -25,7 +28,8 @@ public class OAuthService {
     @Value("${oauth.client.secret}")
     private String CLIENTSECRET;
 
-    private final String URL = "https://github.com/login/oauth/access_token";
+    private final String OAUTHURL = "https://github.com/login/oauth/access_token";
+    private final String EMAILAPIURL = "https://api.github.com/user/emails";
 
     public OAuthGithubToken getAccessToken(String code) {
         MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
@@ -41,13 +45,23 @@ public class OAuthService {
 
         HttpEntity<?> request = new HttpEntity<>(requestPayload, headers);
 
-        ResponseEntity<?> response = new RestTemplate().postForEntity(URL, request, OAuthGithubToken.class);
+        ResponseEntity<?> response = new RestTemplate().postForEntity(OAUTHURL, request, OAuthGithubToken.class);
         return (OAuthGithubToken) response.getBody();
     }
 
-    public String getUserEmailFromOAuthToken(String code) {
+    public ResponseEntity<JsonNode> getUserEmailFromOAuthToken(String accessToken) {
+        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+        Map<String, String> header = new HashMap<>();
+        header.put("Authorization", accessToken);
+        header.put("Content-Type", "application/json");
 
-        return null;
+        headers.setAll(header);
+
+        HttpEntity<?> request = new HttpEntity<>(headers);
+
+        ResponseEntity<JsonNode> response = new RestTemplate().exchange(EMAILAPIURL, HttpMethod.GET, request, JsonNode.class);
+
+        return response;
     }
 
 }
