2901e44f43dbf2bbeae1778fc1569edfcc976e7c
sedin <leesejinn@naver.com>
2020-04-25 00:04:00 +0900

    [#33] Feat: Access Token을 통해 githubEmail 받는 기능 구현
    - 사용자가 header에 Access Token을 넣어 요청
    - Access Token을 담아 github에 사용자 email을 요청
    - github에서 Access Token 유효한지 검사 후 email 반환

--- BE/src/main/java/com/codesquad/sidedish/api/LoginController.java
+++ BE/src/main/java/com/codesquad/sidedish/api/LoginController.java
@@ -1,12 +1,17 @@
 package com.codesquad.sidedish.api;
 
 import com.codesquad.sidedish.entity.OAuthGithubToken;
+import com.codesquad.sidedish.entity.User;
 import com.codesquad.sidedish.service.OAuthService;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.websocket.server.PathParam;
 
@@ -15,19 +20,37 @@ public class LoginController {
 
     private static final Logger log = LoggerFactory.getLogger(SidedishController.class);
 
-    private final OAuthService oAuthService;
+    private final OAuthService oauthService;
 
     public LoginController(OAuthService oAuthService) {
-        this.oAuthService = oAuthService;
+        this.oauthService = oAuthService;
     }
 
     @GetMapping("/login")
     public String OauthTest(@PathParam("code") String code, HttpServletResponse response) {
         log.debug("{}", code);
-        OAuthGithubToken oAuthGithubToken = oAuthService.getAccessToken(code);
+        OAuthGithubToken oAuthGithubToken = oauthService.getAccessToken(code);
         response.setHeader("Authorization", oAuthGithubToken.getAuthorization());
         log.debug("{}", oAuthGithubToken.getAuthorization());
 
         return "/";
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<String> signIn(HttpServletResponse response, HttpServletRequest request) {
+        String accessToken = request.getHeader("Authorization");
+        log.debug("{}", accessToken);
+        ResponseEntity<JsonNode> jsonNode = oauthService.getUserEmailFromOAuthToken(accessToken);
+        JsonNode body = jsonNode.getBody();
+
+        User newUser = null;
+        for (JsonNode child : body) {
+            if(child.get("primary").asText().equals("true")) {
+                newUser = new User(child.get("email").asText());
+            }
+        }
+        log.debug("{}", newUser.getGithubEmail());
+
+        return ResponseEntity.ok(newUser.getGithubEmail());
     }
 }
