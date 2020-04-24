0627944b2ee7d9e543a4366f43fc8a3a059a39ba
sedin <leesejinn@naver.com>
2020-04-24 20:57:41 +0900

    [#33] Feat: 회원가입 버튼 요청 처리를 위한 method 생성
    - POST 회원가입 요청을 위한 method 생성

--- BE/src/main/java/com/codesquad/sidedish/api/LoginController.java
+++ BE/src/main/java/com/codesquad/sidedish/api/LoginController.java
@@ -4,14 +4,13 @@ import com.codesquad.sidedish.entity.OAuthGithubToken;
 import com.codesquad.sidedish.service.OAuthService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.websocket.server.PathParam;
 
-@RestController
+@Controller
 public class LoginController {
 
     private static final Logger log = LoggerFactory.getLogger(SidedishController.class);
@@ -22,13 +21,13 @@ public class LoginController {
         this.oAuthService = oAuthService;
     }
 
-    @GetMapping("/login/oauth2/code/github")
-    public ResponseEntity<String> OauthTest(@PathParam("code") String code, HttpServletResponse response) {
+    @GetMapping("/login")
+    public String OauthTest(@PathParam("code") String code, HttpServletResponse response) {
         log.debug("{}", code);
         OAuthGithubToken oAuthGithubToken = oAuthService.getAccessToken(code);
         response.setHeader("Authorization", oAuthGithubToken.getAuthorization());
         log.debug("{}", oAuthGithubToken.getAuthorization());
 
-        return ResponseEntity.ok("Login Success");
+        return "/";
     }
 }
