2a263705867d62be0ddc65d3cf9cb893dff7cc2a
TTOzzi <devttozzi@gmail.com>
2020-04-26 01:36:12 +0900

    [#42] Refactor: ImageFileManager 를 싱글톤 객체에서 static 클래스로 변경
    ImageFileManager 가 변수 프로퍼티를 가지지 않으므로 static 으로 바꿔줌

--- iOS/SideDish/SideDish/Model/ImageFileManager.swift
+++ iOS/SideDish/SideDish/Model/ImageFileManager.swift
@@ -9,17 +9,15 @@
 import UIKit
 
 class ImageFileManager {
-    static let shared: ImageFileManager = ImageFileManager()
+    static let imageFileExtension = ".jpeg"
     
-    private let imageFileExtension = ".jpeg"
-    
-    func saveImage(image: UIImage, name: String) {
+    static func saveImage(image: UIImage, name: String) {
         guard let data = image.jpegData(compressionQuality: 1),
             let directory = try? FileManager.default.url(for: .cachesDirectory, in: .userDomainMask, appropriateFor: nil, create: false) as URL else { return }
         try? data.write(to: directory.appendingPathComponent(name + imageFileExtension))
     }
     
-    func getSavedImage(name: String) -> UIImage? {
+    static func getSavedImage(name: String) -> UIImage? {
         guard let directory = try? FileManager.default.url(for: .cachesDirectory, in: .userDomainMask, appropriateFor: nil, create: false) else { return nil }
         let path = URL(fileURLWithPath: directory.absoluteString).appendingPathComponent(name + imageFileExtension).path
         let image = UIImage(contentsOfFile: path)
