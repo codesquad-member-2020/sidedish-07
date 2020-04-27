d1e9782d3b3bb17fa70fd8a761c4225ed2ef713d
랄랄랄 <hyewon3938@naver.com>
2020-04-28 03:06:18 +0900

    [#25] slick 라이브러리로 carousel 생성 & axios로 데이터 패치
    - slick과 slick css를 상단에서 import
    - slick의 옵션값을 적용
    - 좌,우 이동 버튼 커스텀
    - 이동 버튼과 캐러셀의 위치 조정
    - carouselItem에 props 전달
    - useEffect로 데이터 받아오는 기능
    - axios 적용

--- FE/sidedish/src/components/Carousel/Carousel.jsx
+++ FE/sidedish/src/components/Carousel/Carousel.jsx
@@ -1,13 +1,125 @@
-import React from 'react';
-import styled from '@emotion/styled';
+// import React from 'react';
+import styled from "@emotion/styled";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import Arrow from "./Arrow";
+import CarouselItem from "./CarouselItem";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { findByLabelText } from "@testing-library/react";
 
 
-const Carousel = () => {
-    return (
-        <div>
-            Carousel
-        </div>
-    );
+const Carousel = (props) => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToScroll: 4,
+    slidesToShow: 4,
+    nextArrow: <Arrow type="next" />,
+    prevArrow: <Arrow type="prev" />,
+  };
+
+  const CarouselWrap = styled.div`
+    width : 100%;
+    display : flex;
+    justify-content : center;
+    margin : 15px;
+  `;
+
+  const CarouselContentWrap = styled.div`
+    width : 1300px;
+    display : flex;
+    justify-content : center;
+    flex-direction : column;
+    text-align : center;
+    padding : 10px 10px 30px 10px;
+    box-sizing : border-box;
+  `;
+
+  const CarouselCategory = styled.div`
+    color : grey;
+    font-size : 20px;
+    margin : 10px 10px 20px 10px;
+    font-weight : bold;
+  `;
+
+  const CarouselTitle = styled.div`
+    color : #424242;
+    font-size : 25px;
+    margin-bottom : 45px;
+    font-weight : bold;
+  `;
+    
+    let sidedish = [];
+
+    let [sideCarousel, setSideCarousel] = useState([]);
+    let [mainCarousel, setMainCarousel] = useState([]);
+    let [soupCarousel, setSoupCarousel] = useState([]);
+
+    useEffect(() => {
+      axios.get("http://15.164.63.83:8080/products/side").then((response) => {
+        setSideCarousel(response.data.content);
+      });
+    }, []);
+
+    useEffect(() => {
+      axios.get("http://15.164.63.83:8080/products/main").then((response) => {
+        setMainCarousel(response.data.content);
+      });
+    }, []);
+
+    useEffect(() => {
+      axios.get("http://15.164.63.83:8080/products/soup").then((response) => {
+        setSoupCarousel(response.data.content);
+
+      });
+    }, []);
+
+    const setData = ()=>{
+        switch(props.category){
+            case "밑반찬" :
+                sidedish = sideCarousel;
+                break;
+            case "메인반찬" :
+                sidedish = mainCarousel;
+                break;
+            case "국·찌개" :
+                sidedish = soupCarousel;
+                break;    
+        }
+    }
+
+  return (
+    <React.Fragment>
+        {setData()}
+      <CarouselWrap>
+          <CarouselContentWrap>
+        <CarouselCategory>{props.category}</CarouselCategory>
+        <CarouselTitle>{props.title}</CarouselTitle>
+        <Slider style={{display : "flex", alignItems : "center"}} {...settings}> 
+        {sidedish.map((list) => {
+        return (
+          <CarouselItem
+            hash = {list.hash}
+            title = {list.title}
+            alt = {list.alt}
+            description = {list.description}
+            salePrice = {list.salePrice}
+            normalPrice = {list.normalPrice}
+            image = {list.image}
+            deliveryTypes = {list.deliveryTypes}
+            badges = {list.badges}
+          />
+        );
+      })}
+        </Slider>
+        </CarouselContentWrap>
+      </CarouselWrap>
+      {    console.log(soupCarousel[5].badges)}
+    </React.Fragment>
+  );
 };
 
 export default Carousel;
