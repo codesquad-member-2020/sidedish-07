3bcfbcfe3d5e9f08291eb236c50ba10feee9c0fb
TTOzzi <devttozzi@gmail.com>
2020-04-28 00:06:59 +0900

    [#45] Feat: 쿠키 삭제 구현
    한번 연동하고 나면 쿠키가 남아 다시 연동화면이 나오지 않는 문제 해결

--- iOS/SideDish/SideDish/Login/LoginViewController.swift
+++ iOS/SideDish/SideDish/Login/LoginViewController.swift
@@ -13,8 +13,10 @@ class LoginViewController: UIViewController, WKNavigationDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        webView.cleanAllCookies()
+        
         guard let url = URL(string: "https://github.com/login?client_id=71186054709e9adda0f9&return_to=%2Flogin%2Foauth%2Fauthorize%3Fclient_id%3D71186054709e9adda0f9%26redirect_uri%3Dhttp%253A%252F%252F15.164.63.83%253A8080%252Flogin%252Foauth2%252Fcode%252Fgithub%26scope%3Duser%253Aemail") else { return }
-        //        guard let url = URL(string: "https://github.com/") else { return }
         let request = URLRequest(url: url)
         webView.navigationDelegate = self
         webView.load(request)
@@ -32,5 +34,15 @@ class LoginViewController: UIViewController, WKNavigationDelegate {
             }
         }
         decisionHandler(.allow)
+    }
+}
+
+extension WKWebView {
+    func cleanAllCookies() {
+        WKWebsiteDataStore.default().fetchDataRecords(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes()) { records in
+            records.forEach { record in
+                WKWebsiteDataStore.default().removeData(ofTypes: record.dataTypes, for: [record], completionHandler: {})
+            }
+        }
     }
 }
