7ca7a922a34a02c4064cb323648d36ca89e30589
Jay <beginin15@gmail.com>
2020-04-29 04:09:48 +0900

    [#53] Refactor: UserRepository를 이용하여 DB에 동일한 이메일이 존재하지 않는 경우 DB에 추가
    - UserRepository : countByGitHubEmail 메소드를 이용하여 인자로 전달된 이메일과 일치하는 데이터가 존재하지 않는 경우 User 데이터를 저장하도록 함.

--- BE/src/main/java/com/codesquad/sidedish/repository/UserRepository.java
+++ BE/src/main/java/com/codesquad/sidedish/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package com.codesquad.sidedish.repository;
 
 import com.codesquad.sidedish.entity.User;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
+
+    @Query("SELECT COUNT(github_email) FROM user WHERE github_email = :githubEmail")
+    public Integer countByGithubEmail(String githubEmail);
 }
