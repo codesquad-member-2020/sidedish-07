0c3b437e00d58916c9c31a540504bb1774dcd685
sedin2 <53131108+sedin2@users.noreply.github.com>
2020-04-28 15:20:24 +0900

    [#33] Feat: JWT Token발급
    - Github 로그인 후 백엔드에서 JWT 토큰 쿠키에 담아 반환

--- BE/src/main/java/com/codesquad/sidedish/api/LoginController.java
+++ BE/src/main/java/com/codesquad/sidedish/api/LoginController.java
@@ -2,18 +2,21 @@ package com.codesquad.sidedish.api;
 
 import com.codesquad.sidedish.entity.OAuthGithubToken;
 import com.codesquad.sidedish.entity.User;
+import com.codesquad.sidedish.response.ResponseData;
+import com.codesquad.sidedish.security.JwtToken;
 import com.codesquad.sidedish.service.OAuthService;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.websocket.server.PathParam;
+import java.io.IOException;
 
 @Controller
 public class LoginController {
@@ -21,13 +24,14 @@ public class LoginController {
     private static final Logger log = LoggerFactory.getLogger(SidedishController.class);
 
     private final OAuthService oauthService;
+    private JwtToken jwtToken = new JwtToken();
 
     public LoginController(OAuthService oAuthService) {
         this.oauthService = oAuthService;
     }
 
     @GetMapping("/login")
-    public ResponseEntity<String> OauthTest(@PathParam("code") String code, HttpServletResponse response) {
+    public String OauthTest(@PathParam("code") String code, HttpServletResponse response) throws IOException {
         log.debug("{}", code);
         OAuthGithubToken oAuthGithubToken = oauthService.getAccessToken(code);
         log.debug("{}", oAuthGithubToken.getAuthorization());
@@ -44,7 +48,11 @@ public class LoginController {
         }
         log.debug("{}", newUser.getGithubEmail());
 
-        return ResponseEntity.ok("Login Success");
+        String jwt = jwtToken.JwtTokenMaker(newUser);
+        log.debug("{}", jwt);
+
+        response.addCookie(new Cookie("Authorization", jwt));
+        return "redirect:/";
     }
 
 }
