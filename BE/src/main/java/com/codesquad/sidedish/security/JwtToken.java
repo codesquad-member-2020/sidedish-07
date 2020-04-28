f1fbdd03050d1c4c60a7fd281df0c925b1cfe8ff
Jay <beginin15@gmail.com>
2020-04-29 04:11:49 +0900

    [#53] Feat: 토큰 decode 메소드 추가
    - JwtToken : 유틸성 클래스로 변경. 유효한 토큰인지 검사, 유효하지 않으면 에러 발생시킴. 유효한 토큰인 경우, claim에 저장한 데이터를 파싱함.

--- BE/src/main/java/com/codesquad/sidedish/security/JwtToken.java
+++ BE/src/main/java/com/codesquad/sidedish/security/JwtToken.java
@@ -1,8 +1,12 @@
 package com.codesquad.sidedish.security;
 
 import com.codesquad.sidedish.entity.User;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.expression.ExpressionException;
 
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
@@ -13,8 +17,11 @@ import java.util.Map;
 
 public class JwtToken {
 
-    public String JwtTokenMaker(User user) {
-        String secretKey = "sidedish";
+    private static final Logger log = LoggerFactory.getLogger(JwtToken.class);
+    private static final String SECRET_KEY = "sidedish";
+    private static final String USER_INFO_KEY = "githubEmail";
+
+    public static String JwtTokenMaker(User user) {
         SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
         String githubEmail = user.getGithubEmail();
 
@@ -22,7 +29,7 @@ public class JwtToken {
         Date now = new Date();
         now.setTime(now.getTime()+expiredTime);
 
-        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(secretKey);
+        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(SECRET_KEY);
         Key key = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
 
         Map<String,Object> header = new HashMap<>();
@@ -33,7 +40,7 @@ public class JwtToken {
 
         payload.put("sub","userInfo");
         payload.put("exp",now);
-        payload.put("githubEmail",githubEmail);
+        payload.put(USER_INFO_KEY,githubEmail);
 
         return Jwts.builder()
                 .setHeader(header)
@@ -41,4 +48,13 @@ public class JwtToken {
                 .signWith(signatureAlgorithm,key)
                 .compact();
     }
+
+    public static String validateToken(String token) throws ExpressionException, JwtException {
+        return (String) Jwts.parser()
+                .setSigningKey(DatatypeConverter.parseBase64Binary(SECRET_KEY))
+                .parseClaimsJws(token).getBody()
+                .get(USER_INFO_KEY);
+    }
+
+    private JwtToken() {}
 }
