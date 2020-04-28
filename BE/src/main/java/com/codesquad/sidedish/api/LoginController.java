b1693dc36e3e60c908c7146f769f2d5c0add5cd3
sedin2 <53131108+sedin2@users.noreply.github.com>
2020-04-28 15:20:24 +0900

    [#33] Feat: JWT Token발급
    - Github 로그인 후 백엔드에서 JWT 토큰 쿠키에 담아 반환

--- BE/src/main/java/com/codesquad/sidedish/api/LoginController.java
+++ BE/src/main/java/com/codesquad/sidedish/api/LoginController.java
@@ -2,6 +2,7 @@ package com.codesquad.sidedish.api;
 
 import com.codesquad.sidedish.entity.OAuthGithubToken;
 import com.codesquad.sidedish.entity.User;
+
 import com.codesquad.sidedish.security.JwtToken;
 import com.codesquad.sidedish.service.OAuthService;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -10,8 +11,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import javax.servlet.http.Cookie;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.websocket.server.PathParam;
 import java.io.IOException;
