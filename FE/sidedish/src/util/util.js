5a296d2c932344cb6d2a16f42982240500591e14
Jay <beginin15@gmail.com>
2020-05-06 18:16:00 +0900

    [Review] 백엔드 폴더만 분리

--- FE/sidedish/src/util/util.js
+++
@@ -1,7 +0,0 @@
-const preventScroll = (element,boolean) => {
-    let scrollProperty;
-    boolean === true ? (scrollProperty = "hidden") : (scrollProperty = "auto");
-    element.style.overflow = scrollProperty;
-  };
-
-  export default preventScroll;
